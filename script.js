900145ad51320d80fb8b3805bc2989530eb8737d
oBLAZERo2001 <72096510+oBLAZERo2001@users.noreply.github.com>
2021-05-28 17:30:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+$('#view-work').on('click', function() {
+    const images = $('#images').positio().top;
+
+
+    $('html, body').animate({
+            scrollTop: images
+        },
+        900
+    );
+});
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+})
